b960a99c4644aa6b954fbc387547dba12778e6af
HarryPotter <1208314139@qq.com>
2022-11-01 10:46:50 +0800

    add evaluation process in DiffKS

--- cogagent/models/diffks_model.py
+++ cogagent/models/diffks_model.py
@@ -16,18 +16,86 @@ class DiffKSModel(BaseModel):
 
         vocab_size = len(vocab["vocab_list"])
         unk_id = self.vocab["word_vocab"].label2id("<unk>")
+        self.max_sent_length = 512
 
         self.embLayer = EmbeddingLayer(vocab_size, embedding_size, drop_out,
                                        glove_path=glove_path, vocab_list=vocab["vocab_list"])
         self.postEncoder = PostEncoder(embedding_size,eh_size)
         self.wikiEncoder = WikiEncoder(embedding_size,eh_size)
         self.connectLayer = ConnectLayer(eh_size,dh_size,hist_len,hist_weights)
-        self.genNetwork = GenNetwork(embedding_size,eh_size,dh_size,vocab_size,unk_id,drop_out)
+        self.genNetwork = GenNetwork(embedding_size,eh_size,dh_size,vocab_size,unk_id,drop_out,vocab)
 
     def loss(self, batch, loss_function):
         sent_loss,atten_loss = self.forward(batch)
 
         return sent_loss + atten_loss
+
+    def evaluate(self, batch, metric_function):
+        resp = batch["resp"]
+        sent_num = batch["sent_num"]
+        resp_all_vocabs = batch["resp_all_vocabs"]
+        w_o_all = self.detail_forward(batch)
+
+
+    def detail_forward(self,batch):
+        """
+        forward process for evaluation
+        :param batch:
+        :return:
+        """
+        post,resp,wiki = batch["post"],batch["resp"],batch["wiki"]
+        post_length,resp_length = batch["post_length"],batch["resp_length"]
+        wiki_length = batch["wiki_length"]
+        device = batch["post"].device
+        dialogue_turns = torch.max(batch["sent_num"]).item()
+        dm = self.vocab
+        go_id = self.vocab["word_vocab"].label2id("<go>")
+        eos_id = self.vocab["word_vocab"].label2id("<eos>")
+        pad_id = self.vocab["word_vocab"].label2id("<pad>")
+
+        def pad_post(posts):
+            '''
+            :param posts: list, [batch, turn_length, sent_length]
+            '''
+            post_length = np.array(list(map(len, posts)), dtype=int)
+            post = np.zeros((len(post_length), np.max(post_length)), dtype=int)
+            for j in range(len(post_length)):
+                post[j, :len(posts[j])] = posts[j]
+            return post, post_length
+
+        ori_post = [[single_post[single_post != pad_id].tolist() for single_post in posts] for posts in post]
+        new_post = [each[0] for each in ori_post]
+
+        for i in range(dialogue_turns):
+            state_num = i
+            curr_post,curr_resp,curr_wiki = post[:,i,:],resp[:,i,:],wiki[:,i,:,:]
+            curr_post_length,curr_resp_length = post_length[:,i], resp_length[:,i]
+            curr_wiki_length = wiki_length[:,i]
+            curr_wiki_num = batch["wiki_num"][:,i]
+            curr_post,curr_post_length = [torch.from_numpy(elem).to(device) for elem in pad_post(new_post)]
+            curr_post_embedding,curr_resp_embedding,curr_wiki_embedding = self.embLayer.detail_forward(curr_post),\
+                                                           self.embLayer.detail_forward(curr_resp),\
+                                                           self.embLayer.detail_forward(curr_wiki)
+            h, hn, valid_sen, reverse_valid_sen = self.postEncoder.detail_forward(curr_post_embedding, curr_post_length)
+            h1, hn1 = self.wikiEncoder(curr_wiki_embedding, curr_wiki_length)
+            if not self.disentangle:
+                selected_wiki_sen,selected_wiki_h,init_h,wiki_cv,single_atten_loss,(acc_prob,acc_label,acc_pred) = self.connectLayer.detail_forward()
+            else:
+                selected_wiki_sen,selected_wiki_h,init_h,wiki_cv,single_atten_loss,(acc_prob,acc_label,acc_pred) = self.connectLayer.detail_forward_disentangle(
+                    i,valid_sen,reverse_valid_sen,curr_wiki,curr_wiki_num,h1,hn1,batch["atten"][:,i],h,hn
+                )
+            w_o_all = self.genNetwork.detail_forward(i,valid_sen,reverse_valid_sen,selected_wiki_sen,selected_wiki_h,
+                                    curr_resp,curr_resp_length,curr_resp_embedding,init_h,wiki_cv,self.embLayer.embedding)
+
+
+            if i < dialogue_turns - 1:
+                gen_resp = w_o_all[-1].transpose(0, 1).cpu().tolist()
+                new_post = []
+                for j, gr in enumerate(gen_resp):
+                    gr = np.array(gr)
+                    new_post.append((ori_post[j][i] + ([go_id] + gr[gr!=pad_id].tolist() + [eos_id])[:self.max_sent_length] + ori_post[j][i + 1])[-100:])
+
+        return w_o_all
 
     def forward(self, batch):
         post,resp,wiki = batch["post"],batch["resp"],batch["wiki"]
@@ -76,6 +144,9 @@ class EmbeddingLayer(nn.Module):
         """
         return self.dropout(self.embedding(data))
 
+    def detail_forward(self,data):
+        return self.embedding(data)
+
 
 class PostEncoder(nn.Module):
     def __init__(self, embedding_size, eh_size):
@@ -89,6 +160,19 @@ class PostEncoder(nn.Module):
         :param post_length:(batch_size,)
         :return:
         """
+        valid_sen =torch.sum(torch.nonzero(raw_post_length),1) # (batch_size,)
+        raw_reverse = torch.cumsum(torch.gt(raw_post_length, 0), 0) - 1 # (batch_size)
+        reverse_valid_sen = raw_reverse * torch.ge(raw_reverse, 0).to(torch.long) # (batch_size)
+        valid_num = valid_sen.shape[0]
+
+        post = torch.index_select(raw_post, 0, valid_sen).transpose(0, 1)  # (utterance_sequence_length, valid_num, embedding_dim)
+        post_length = torch.index_select(raw_post_length, 0, valid_sen)
+        post_length = post_length.cpu().numpy()
+
+        h,hn = self.postGRU.forward(post, post_length, need_h=True)
+        return h,hn,valid_sen,reverse_valid_sen
+
+    def detail_forward(self,raw_post,raw_post_length):
         valid_sen =torch.sum(torch.nonzero(raw_post_length),1) # (batch_size,)
         raw_reverse = torch.cumsum(torch.gt(raw_post_length, 0), 0) - 1 # (batch_size)
         reverse_valid_sen = raw_reverse * torch.ge(raw_reverse, 0).to(torch.long) # (batch_size)
@@ -149,6 +233,9 @@ class ConnectLayer(nn.Module):
     def forward(self):
         return None,None,None,None
 
+    def detail_forward(self):
+        return None,None,None,None,None,[None,None,None]
+
     def forward_disentangle(self,i,valid_sen,reverse_valid_sen,wiki_sen,wiki_num,h1,h_n1,atten_label,h,h_n):
         valid_wiki_h_n1 = torch.index_select(h_n1, 1, valid_sen)
         valid_wiki_sen = torch.index_select(wiki_sen, 0, valid_sen)
@@ -204,9 +291,72 @@ class ConnectLayer(nn.Module):
 
         return selected_wiki_sen,selected_wiki_h,init_h,wiki_cv,atten_loss
 
+    def detail_forward_disentangle(self,i,valid_sen,reverse_valid_sen,wiki_sen,wiki_num,h1,h_n1,atten_label,h,h_n):
+        valid_wiki_h_n1 = torch.index_select(h_n1, 1, valid_sen)
+        valid_wiki_sen = torch.index_select(wiki_sen, 0, valid_sen)
+        valid_wiki_h1 = torch.index_select(h1,1,valid_sen)
+        atten_label = torch.index_select(atten_label,0,valid_sen)
+        valid_wiki_num = torch.index_select(wiki_num,0,valid_sen)
+
+        self.beta = torch.sum(valid_wiki_h_n1 * h_n, dim = 2).T # (batch_size_valid,wiki_len)
+        mask = torch.arange(self.beta.shape[1], device=self.beta.device).long().expand(self.beta.shape[0],self.beta.shape[1]).transpose(0,1)
+        expand_wiki_num = valid_wiki_num.unsqueeze(0).expand_as(mask)
+        reverse_mask = (expand_wiki_num <= mask).float()
+
+        if i > 0:
+            h2,hn_2 = self.compareGRU(h_n1,wiki_num.cpu().numpy(),need_h=True)
+            valid_wiki_h2 = torch.index_select(h2, 1, valid_sen) # (wiki_len,batch_valid_num,2 * eh_size)
+
+            tilde_wiki_list = []
+            for i in range(self.last_wiki.size(-1)):
+                last_wiki = torch.index_select(self.last_wiki[:, :, i], 0, valid_sen).unsqueeze(
+                    0)  # 1, valid_num, (2 * eh)
+                tilde_wiki = torch.tanh(
+                    self.tilde_linear(torch.cat([last_wiki - valid_wiki_h2, last_wiki * valid_wiki_h2], dim=-1)))
+                tilde_wiki_list.append(tilde_wiki.unsqueeze(-1) * self.hist_weights[i])
+            tilde_wiki = torch.cat(tilde_wiki_list, dim=-1).sum(dim=-1)
+            # wiki_len * valid_num * (2 * eh_size)
+
+            query = self.attn_query(tilde_wiki)  # [1, valid_num, hidden]
+            key = self.attn_key(torch.cat([valid_wiki_h2, tilde_wiki], dim=-1))  # [wiki_sen_num, valid_num, hidden]
+            atten_sum = self.attn_v(torch.tanh(query + key)).squeeze(-1)  # [wiki_sen_num, valid_num]
+
+            self.beta = self.beta[:, :atten_sum.shape[0]] + torch.t(atten_sum)
+
+        atten_loss = self.atten_lossCE(self.beta,atten_label)
+        atten_loss = torch.zeros_like(atten_loss) if torch.isnan(atten_loss).detach().cpu().numpy() > 0 else atten_loss
+
+        self.beta = torch.t(self.beta) - 1e10 * reverse_mask[:self.beta.shape[1]]
+        self.alpha = self.wiki_atten(self.beta)  # wiki_len * valid_num
+        acc_prob = torch.index_select(self.alpha.t(), 0, reverse_valid_sen).cpu().tolist()
+        atten_indices = torch.argmax(self.alpha, 0) # valid_num
+        alpha =  torch.zeros_like(self.beta.T).scatter_(1, atten_indices.unsqueeze(1), 1)
+        alpha = alpha.T
+
+        wiki_cv = torch.sum(valid_wiki_h_n1[:alpha.shape[0]] * alpha.unsqueeze(2), dim=0) # (batch_valid_num,2*eh_size)
+        init_h = self.initLinearLayer(torch.cat([h_n, wiki_cv], 1)) # (batch_valid_num,2*eh_size)
+
+        if i == 0:
+            self.last_wiki = torch.index_select(wiki_cv, 0, reverse_valid_sen).unsqueeze(-1)  # [batch, 2 * eh_size]
+        else:
+            self.last_wiki = torch.cat([torch.index_select(wiki_cv, 0, reverse_valid_sen).unsqueeze(-1),
+                                        self.last_wiki[:, :, :self.hist_len-1]], dim=-1)
+
+        acc_label = torch.index_select(atten_label, 0, reverse_valid_sen).cpu().tolist()
+        acc_pred = torch.index_select(atten_indices, 0, reverse_valid_sen).cpu().tolist()
+
+        atten_indices = atten_indices.unsqueeze(1)
+        atten_indices = torch.cat([torch.arange(atten_indices.shape[0]).unsqueeze(1), atten_indices.cpu()], 1) # (batch_valid_num,1)
+        valid_wiki_h1 = torch.transpose(valid_wiki_h1, 0, 1) # (batch_valid_num, wiki_len, wiki_num,(2 * eh_size))
+        valid_wiki_h1 = torch.transpose(valid_wiki_h1, 1, 2) # (batch_valid_num, wiki_num ,wiki_sen_len, (2 * eh_size))
+        selected_wiki_h = valid_wiki_h1[atten_indices.chunk(2, 1)].squeeze(1)
+        selected_wiki_sen = valid_wiki_sen[atten_indices.chunk(2, 1)].squeeze(1)
+
+        return selected_wiki_sen,selected_wiki_h,init_h,wiki_cv,atten_loss,[acc_prob,acc_label,acc_pred]
+
 
 class GenNetwork(nn.Module):
-    def __init__(self, embedding_size,eh_size,dh_size,vocab_size,unk_id,droprate):
+    def __init__(self, embedding_size,eh_size,dh_size,vocab_size,unk_id,droprate,vocab):
         super(GenNetwork, self).__init__()
         self.GRULayer = MyGRU(embedding_size + 2 * eh_size, dh_size, initpara=False)
         self.wLinearLayer = nn.Linear(dh_size, vocab_size)
@@ -216,6 +366,9 @@ class GenNetwork(nn.Module):
         self.start_generate_id = 2
         self.vocab_size = vocab_size
         self.max_sent_length = 512
+        self.vocab = vocab
+
+        self.w_o_all = None
 
     def forward(self,i,valid_sen,reverse_valid_sen,
                 selected_wiki_sen,selected_wiki_h,
@@ -280,6 +433,93 @@ class GenNetwork(nn.Module):
         gen_p = gen_p / torch.unsqueeze(torch.sum(gen_p, 2), 2)
         gen_p = torch.clamp(gen_p, 1e-10, 1.0)
         return gen_w,gen_p
+
+
+    def detail_forward(self,i,valid_sen,reverse_valid_sen,
+                selected_wiki_sen,selected_wiki_h,
+                curr_resp,curr_resp_length,curr_resp_embedding,
+                init_h,wiki_cv,embLayer):
+        gen_w_o,gen_emb,gen_length,gen_h_n = self.free_run(i,valid_sen,reverse_valid_sen,
+                selected_wiki_sen,selected_wiki_h,
+                curr_resp,curr_resp_length,curr_resp_embedding,
+                init_h,wiki_cv,embLayer)
+        # dm = self.param.volatile.dm
+        w_o = gen_w_o.detach().cpu().numpy()
+
+        w_o_slice = torch.index_select(gen_w_o, 1, reverse_valid_sen)
+        if w_o_slice.shape[0] < self.max_sent_length:
+            w_o_slice = torch.cat([w_o_slice, torch.zeros(self.max_sent_length - w_o_slice.shape[0], w_o_slice.shape[1],device=gen_w_o.device,dtype=torch.long)], 0)
+
+        if i == 0:
+            self.w_o_all = w_o_slice.unsqueeze(0)
+        else:
+            self.w_o_all = torch.cat([self.w_o_all, w_o_slice.unsqueeze(0)], 0) #state.w_all: sen_num * sen_length * batch_size
+        return self.w_o_all
+
+    def free_run(self,i,valid_sen,reverse_valid_sen,
+                selected_wiki_sen,selected_wiki_h,
+                curr_resp,curr_resp_length,curr_resp_embedding,
+                init_h,wiki_cv,embLayer,mode='max'):
+        batch_size = len(valid_sen)
+        device = next(embLayer.parameters()).device
+        go_id = self.vocab["word_vocab"].label2id("<go>")
+        eos_id = self.vocab["word_vocab"].label2id("<eos>")
+        first_emb = embLayer(torch.tensor([go_id],
+                                          dtype=torch.long,device=device)).repeat(batch_size,1)
+        gen_w_pro = []
+        gen_w_o = []
+        gen_emb = []
+        flag = torch.zeros(batch_size,device=device).byte()
+        EOSmet = []
+
+        selected_wiki_sen = selected_wiki_sen[:,:selected_wiki_h.shape[1]]
+        copyHead = torch.zeros(1, selected_wiki_sen.shape[0], selected_wiki_sen.shape[1],
+                    self.vocab_size, dtype=torch.float32, device=selected_wiki_sen.device).scatter_(3,
+                                                                                    torch.unsqueeze(
+                                                                                        torch.unsqueeze(
+                                                                                            selected_wiki_sen,
+                                                                                            0),
+                                                                                3), 1)
+        wikiState = torch.transpose(torch.tanh(self.wCopyLinear(selected_wiki_h)), 0, 1)
+        next_emb = first_emb
+        gru_h = init_h
+        gen_p = []
+        for _ in range(self.max_sent_length):
+            now = torch.cat([next_emb, wiki_cv], dim=-1)
+
+            gru_h = self.GRULayer.cell_forward(now, gru_h)
+            w = self.wLinearLayer(gru_h)
+            w = torch.clamp(w, max=5.0)
+            vocab_p = torch.exp(w)
+            copyW = torch.exp(
+                torch.clamp(torch.unsqueeze((torch.sum(torch.unsqueeze(gru_h, 0) * wikiState, -1).transpose_(0, 1)), 1),
+                            max=5.0))  # batch * 1 * wiki_len
+            copy_p = torch.matmul(copyW, copyHead).squeeze()
+
+            p = vocab_p + copy_p + 1e-10
+            p = p / torch.unsqueeze(torch.sum(p, 1), 1)
+            p = torch.clamp(p, 1e-10, 1.0)
+            gen_p.append(p)
+
+            if mode == "max":
+                w_o = torch.argmax(p[:, self.start_generate_id:], dim=1) + self.start_generate_id
+                next_emb = embLayer(w_o)
+            elif mode == "gumbel":
+                pass
+            gen_w_o.append(w_o)
+            gen_emb.append(next_emb)
+
+            EOSmet.append(flag)
+            flag = flag | (w_o == eos_id).byte()
+            if torch.sum(flag).detach().cpu().numpy() == batch_size:
+                break
+
+        EOSmet = 1 - torch.stack(EOSmet)
+        gen_w_o = torch.stack(gen_w_o) * EOSmet.long()
+        gen_emb = torch.stack(gen_emb) * EOSmet.float().unsqueeze(-1)
+        gen_length = torch.sum(EOSmet, 0).detach().cpu().numpy()
+        gen_h_n = gru_h
+        return gen_w_o,gen_emb,gen_length,gen_h_n
 
 
 def flattenSequence(data, length):
