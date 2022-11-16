00fc2a6869f08cfb7064333b31e9d3b41b9cc42b
HarryPotter <1208314139@qq.com>
2022-11-16 11:35:40 +0800

    update toolkit

--- cogagent/toolkits/knowledge_grounded_dialogue_toolkit.py
+++ cogagent/toolkits/knowledge_grounded_dialogue_toolkit.py
@@ -15,7 +15,7 @@ import numpy as np
 import torch
 
 class KnowledgeGroundedConversationAgent(BaseToolkit):
-    def __init__(self,bert_model,model_path,vocabulary_path,device):
+    def __init__(self,bert_model,model_path,vocabulary_path,device,debug=False):
         super(KnowledgeGroundedConversationAgent, self).__init__(bert_model,model_path,vocabulary_path,device)
         if bert_model:
             self.plm = PlmAutoModel(bert_model)
@@ -24,12 +24,14 @@ class KnowledgeGroundedConversationAgent(BaseToolkit):
         self.topic_choice_count = ['A','B','C','D','E']
         vocab = {"word_vocab":self.vocabulary}
 
+        self.debug = debug
+
         self.max_token_len = 100
         self.word_vocab = self.vocabulary
         self.word2id= self.word_vocab.get_label2id_dict()
         self.id2word = self.word_vocab.get_id2label_dict()
         self.unk_id,self.eos_id = self.word2id["<unk>"],self.word2id["<eos>"]
-        self.go_id = self.word2id["<go>"]
+        self.go_id,self.pad_id = self.word2id["<go>"],self.word2id["<pad>"]
         self.line2id = lambda line: ([self.go_id] + list(map(lambda word: self.word2id.get(word, self.unk_id), line)) + [self.eos_id])[:self.max_token_len]
         self.know2id = lambda line: (list(map(lambda word: self.word2id.get(word, self.unk_id), line)))[:self.max_token_len]
         self.id2line = lambda line: list(map(lambda id:self.id2word.get(id,"<unk>"),line))
@@ -38,6 +40,7 @@ class KnowledgeGroundedConversationAgent(BaseToolkit):
         self.max_wiki_num = 280
         self.max_wiki_length = 256
         self.max_post_length = 128
+        self.max_sent_length = 512
         self.valid_vocab_len = len(self.vocabulary)
 
         self.model = DiffKSModel(vocab,glove_path=None)
@@ -48,19 +51,17 @@ class KnowledgeGroundedConversationAgent(BaseToolkit):
         self.type2type[float] = torch.float
         self.type2type[bool] = torch.bool
 
-    def run(self,debug=False):
-        topic = self.choose_topic(debug)
+    def run(self):
+        topic = self.choose_topic()
         padded_wiki,wiki_length,wiki_num = self.search_knowledge(topic)
         dialogue_turns = int(padded_wiki.shape[0])
 
         chat_history = []
+
+        input_msg = self.get_user_input()
+        post = [self.line2id(self.f(input_msg).split())[:self.max_post_length]]
+
         for step in range(dialogue_turns):
-            if debug:
-                input_msg = "Let's talk about {}!".format(topic)
-                print(">>User:{}".format(input_msg))
-            else:
-                input_msg = input(">>User")
-            post = [self.line2id(self.f(input_msg).split())[:self.max_post_length]]
             curr_post, curr_post_length = [elem for elem in pad_post(post)]
             input_batch = self.convert_to_sensor({
                 "curr_post":curr_post[0].tolist(),
@@ -74,8 +75,20 @@ class KnowledgeGroundedConversationAgent(BaseToolkit):
             gen_resp = self.model.predict(input_batch)
             gen_resp_str = " ".join(self.id2line(gen_resp[0]))
             print(">>Agent:",gen_resp_str)
-            break
+            if step < dialogue_turns - 1:
+                input_msg = self.get_user_input()
+                gr = np.array(gen_resp[0])
+                post = [(post[0] + ([self.go_id] + gr[gr!=self.pad_id].tolist() + [self.eos_id])[:self.max_sent_length] + self.line2id(self.f(input_msg).split())[:self.max_post_length])[-100:]]
+            # break
+        print(">>Agent:Reach max number of conversations!Bye!")
 
+    def get_user_input(self,):
+        if self.debug:
+            input_msg = "Let's talk about it!"
+            print(">>User:{}".format(input_msg))
+        else:
+            input_msg = input(">>User:")
+        return input_msg
 
     def search_knowledge(self,topic):
         knowledge = self.topic2knowledge[topic]
@@ -108,14 +121,14 @@ class KnowledgeGroundedConversationAgent(BaseToolkit):
         padded_wiki[padded_wiki >= self.valid_vocab_len] = self.unk_id
         return padded_wiki,wiki_length,wiki_num
 
-    def choose_topic(self,debug=False):
+    def choose_topic(self):
         while True:
             print("Please choose one topic:")
             shuffle(self.topics)
             for i,choice in enumerate(self.topic_choice_count[:-1]):
                 print("{}.{}".format(choice,self.topics[i]))
             print("{}.{}".format(self.topic_choice_count[-1],"I want to choose other topics"))
-            if debug:
+            if self.debug:
                 choice = 'B'
             else:
                 choice = input(">>")
@@ -171,8 +184,9 @@ if __name__ == '__main__':
         model_path='/data/hongbang/CogAGENT/datapath/knowledge_grounded_dialogue/wow/experimental_result/run_diffks_wow--2022-11-13--04-17-09.20/best_model/checkpoint-9215/models.pt',
         vocabulary_path='/data/hongbang/CogAGENT/datapath/knowledge_grounded_dialogue/wow/cache/wow_vocab.pkl',
         device=torch.device("cuda:0"),
+        debug=False,
         )
-    agent.run(debug=True)
+    agent.run()
 
     # topic = agent.choose_topic()
     # print(topic)
